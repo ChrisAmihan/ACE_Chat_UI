f95906d15200d9db4c0e8a9936d6c49524c6cb6c
Chris Amihan <chris.amihan@revature.net>
2021-10-10 13:16:46 -0400

    new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ContentComponent } from './content/content.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: "login", pathMatch:"full"},
+  {path:"login", component:LoginComponent},
+  {path:"content", component:ContentComponent},
+  {path:"new-user", component:NewUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AceLoginComponent } from './ace-login/ace-login.component';
-import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { LoginComponent } from './login/login.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
+import { MessageBoxComponent } from './message-box/message-box.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ChatListComponent } from './chat-list/chat-list.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AceLoginComponent
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    NewUserComponent,
+    LoginComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent,
+    MessageBoxComponent,
+    SearchBarComponent,
+    ChatListComponent,
+    ChatWindowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
